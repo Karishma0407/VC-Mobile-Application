7d54d67526a9c8a09d1d7ed42235d0a5614c2936
Karishma <69646809+Karishma0407@users.noreply.github.com>
2024-02-20 10:52:15 -0500

    MainActivity updated

--- Lab4/StudentGradeProject/app/src/main/java/com/example/studentgradeproject/MainActivity.java
+++ Lab4/StudentGradeProject/app/src/main/java/com/example/studentgradeproject/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.studentgradeproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Use FragmentManager to add StudentGradeFragment to fragment_container
+        //of MainActivity
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if(fragment == null)
+        {
+            fragment = new StudentGradeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 }
