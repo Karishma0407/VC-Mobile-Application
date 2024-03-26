fbbdbf38673a832231e415315fe1a52dc1becabd
Karishma0407 <karishma0407@icloud.com>
2024-03-25 20:54:00 -0400

    laptop commit

--- Lab5/BookPublishingProject/app/src/main/java/com/example/bookpublishingproject/MainActivity.java
+++ Lab5/BookPublishingProject/app/src/main/java/com/example/bookpublishingproject/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.bookpublishingproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView pubIdTextView;
+    private TextView pubNameTextView;
+    private TextView pubAddTextView;
+    private Button prevPubButton;
+    private Button nextPubButton;
+    private Button pubDetailsButton;
+    private Button showBookButton;
+
+    private int currentIndex = 0;
+
+    public Publisher[] all_publishers;
+    Context context;
+    ArrayList<Publisher> publisherModelArrayList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Publisher publisher1 = new Publisher();
+        Publisher publisher2 = new Publisher();
+        Publisher publisher3 = new Publisher();
+        Publisher publisher4 = new Publisher();
+        Publisher publisher5 = new Publisher();
+
     }
 }
